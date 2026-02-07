babf490a2e802dc2b7add8a19a9bbe6fbadb9260
“Efe <2847709d@student.gla.ac.uk>
2026-02-07 21:46:04 +0000

    Chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from rango import views
 
 app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('add_category/', views.add_category, name='add_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from rango.models import Category
+from rango.forms import CategoryForm
 
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
